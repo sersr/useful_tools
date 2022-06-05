a7834704b27bba6a7befa477f75d181d17e52b80
sersr <1334349436@qq.com>
2022-06-05 19:39:28 +0800

    顺序：从上下文查找，再从页面查找

--- lib/src/navigation/nop.dart
+++ lib/src/navigation/nop.dart
@@ -10,20 +10,43 @@ typedef NopWidgetBuilder = Widget Function(BuildContext context, Widget child);
 typedef NopPreInitCallback = void Function(
     T? Function<T>({bool shared}) preInit);
 
+extension GetType on BuildContext {
+  T getType<T>({bool shared = true}) {
+    return Nop.of(this, shared: shared);
+  }
+
+  T? getTypeOr<T>({bool shared = true}) {
+    return Nop.maybyOf(this, shared: shared);
+  }
+}
+
 /// 当前共享对象的存储位置
-class Nop extends StatefulWidget {
+class Nop<C> extends StatefulWidget {
   const Nop({
     Key? key,
     required this.child,
     this.builder,
     this.preRun,
     this.builders,
-  }) : super(key: key);
+    this.create,
+  })  : value = null,
+        super(key: key);
+  const Nop.value({
+    Key? key,
+    this.value,
+    required this.child,
+    this.builder,
+    this.preRun,
+    this.builders,
+  })  : create = null,
+        super(key: key);
 
   final Widget child;
   final NopPreInitCallback? preRun;
   final NopWidgetBuilder? builder;
   final List<NopWidgetBuilder>? builders;
+  final C Function(BuildContext context)? create;
+  final C? value;
 
   static T of<T>(BuildContext context, {bool shared = true}) {
     final nop = context.dependOnInheritedWidgetOfExactType<_NopScoop>()!;
@@ -35,29 +58,83 @@ class Nop extends StatefulWidget {
     return nop?.state.getType<T>(context, shared: shared);
   }
 
+  static _NopState? _maybyOf(BuildContext context) {
+    final nop = context.dependOnInheritedWidgetOfExactType<_NopScoop>();
+    return nop?.state;
+  }
+
   @override
-  State<Nop> createState() => _NopState();
+  State<Nop<C>> createState() => _NopState<C>();
 }
 
-class _NopState extends State<Nop> {
+class _NopState<C> extends State<Nop<C>> {
   final _caches = HashMap<Type, NopListener>();
   late final _getCacheStack = <Type, Set<String>>{};
 
   T getType<T>(BuildContext context, {bool shared = true}) {
-    var listener = _caches[T];
+    var listener = _createOrFromParent<T>(context);
+
     assert(() {
-      final stack = _getCacheStack.putIfAbsent(T, () => <String>{});
-      stack.add('shared: $shared > ' + Log.getLineFromStack(position: 4));
+      if (listener != null) {
+        final stack = _getCacheStack.putIfAbsent(T, () => <String>{});
+        stack.add('shared: $shared > ' + Log.getLineFromStack(position: 4));
+      }
       return true;
     }());
+
     if (listener == null) {
       listener = Nav.getType<T>(context, shared: shared);
-
       listener.add(this);
       _caches[T] = listener;
     }
-    assert(!shared && Nav.isCurrent<T>(), Log.e(_getCacheStack[T]));
+
+    assert(shared || Nav.isCurrent<T>(), Log.e(_getCacheStack[T]));
     return listener.data;
+  }
+
+  NopListener? _createOrFromParent<T>(BuildContext context) {
+    var listener = _caches[T];
+
+    if (listener == null) {
+      listener = _create<T>();
+      if (listener == null) {
+        final parentState = Nop._maybyOf(this.context);
+        if (parentState != null) {
+          listener = parentState._createOrFromParent<T>(context);
+        }
+      }
+      if (listener != null) {
+        listener.add(this);
+        _caches[T] = listener;
+      }
+    }
+    return listener;
+  }
+
+  NopListener? _create<T>() {
+    if (widget.create != null && T == C) {
+      final data = widget.create!(context);
+      if (data != null) {
+        if (data is NopLifeCycle) data.init();
+        return NopListener(data, _empty);
+      }
+    }
+    return null;
+  }
+
+  static _empty() {}
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.value != null) {
+      final listener = NopListener(widget.value!, _empty);
+      final data = listener.data;
+      if (data is NopLifeCycle) {
+        data.init();
+      }
+      _caches[C] = listener;
+    }
   }
 
   @override
