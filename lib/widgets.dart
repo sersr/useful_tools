6065552532581547a0c9b9db14cbe7c25bd3195d
sersr <1334349436@qq.com>
2024-12-04 22:49:32 +0800

    fix

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -2,4 +2,3 @@ export 'src/widgets/botton.dart';
 export 'src/widgets/image_callback_provider.dart';
 export 'src/widgets/image_future.dart';
 export 'src/widgets/list_builder.dart';
-export 'src/widgets/slide_page_transition.dart';
